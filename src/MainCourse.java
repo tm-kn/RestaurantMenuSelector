f3a84eb76fccc43925fbbbcba420dda7821aa689
tmkn8 <tmkn8@protonmail.ch>
2016-02-19 18:54:22 +0000

    Initial commit

---
+++ src/MainCourse.java
@@ -0,0 +1,32 @@
+/**
+ * It is a type of a course.
+ * @author Tomasz Knapik <u1562595@unimail.hud.ac.uk>
+ *
+ */
+public class MainCourse extends Course {
+	
+	/**
+	 * Contructor inherits from the superclass.
+	 * @param name
+	 * @param price
+	 * @param calories
+	 * @param description
+	 * @param nutFree
+	 * @param vegan
+	 * @param vegetarian
+	 * @param glutenFree
+	 */
+	public MainCourse(String name, Double price, int calories, String description, Boolean nutFree, Boolean vegan,
+			Boolean vegetarian, Boolean glutenFree) {
+		super(name, price, calories, description, nutFree, vegan, vegetarian, glutenFree);
+	}
+	
+	/**
+	 * Course type name.
+	 * @return
+	 */
+	public static String getCourseTypeNameOfClass() {
+		return "Main courses";
+	}
+	
+}
