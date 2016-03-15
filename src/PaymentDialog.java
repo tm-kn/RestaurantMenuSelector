47c1b813509e8d01bcda62c18f68b117cae17197
Tomasz Knapik <tmkn0808@gmail.com>
2016-03-15 14:40:27 +0000

    Add printing receipts and paying by cash

--- src/PaymentDialog.java
+++ src/PaymentDialog.java
@@ -7,11 +7,15 @@ import java.awt.GraphicsConfiguration;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
@@ -25,6 +29,7 @@ public class PaymentDialog extends JDialog {
 	private ButtonGroup paymentMethodRadioGroup;
 	private JRadioButton cardPaymentRadioButton, cashPaymentRadioButton;
 	private JPanel paymentMethodPane, centrePane, paymentFormPane;
+	private JLabel errorMessageLabel;
 	
 	public PaymentDialog(OrderScreen parent) {
 		// Make the dialog modal
@@ -77,13 +82,18 @@ public class PaymentDialog extends JDialog {
 		this.paymentMethodPane.add(this.cashPaymentRadioButton);
 		this.paymentMethodPane.add(this.cardPaymentRadioButton);
 		
+		// Set up error message label
+		this.errorMessageLabel = new JLabel();
 		
 		// Payment form pane
 		this.paymentFormPane = new JPanel();
 		this.paymentFormPane.setLayout(new BoxLayout(this.paymentFormPane, BoxLayout.Y_AXIS));
 		
+		
 		// Add things to the centre pane
 		this.centrePane.add(this.paymentMethodPane);
+		this.centrePane.add(this.errorMessageLabel);
+		this.centrePane.add(this.paymentFormPane);
 		
 		// Add things to the main pane
 		this.cp.add(this.centrePane, BorderLayout.CENTER);
@@ -96,6 +106,8 @@ public class PaymentDialog extends JDialog {
 	}
 	
 	private void refreshPaymentFormPane() {
+		this.paymentFormPane.removeAll();
+		
 		if(this.cashPaymentRadioButton.isSelected()) {
 			this.paymentFormPane.add(this.getCashPaymentForm());
 			
@@ -105,11 +117,15 @@ public class PaymentDialog extends JDialog {
 			this.paymentFormPane.removeAll();
 			this.paymentFormPane.revalidate();
 		}
+		
+		this.paymentFormPane.revalidate();
+		this.paymentFormPane.repaint();
+		this.pack();
 	}
 	
 	private JPanel getCashPaymentForm() {
 		JPanel form = new JPanel();
-		form.setLayout(new BorderLayout());
+		form.setLayout(new FlowLayout());
 		
 		JTextField inputAmount = new JTextField("Amount");
 		
@@ -121,17 +137,26 @@ public class PaymentDialog extends JDialog {
 				PaymentDialog.this.amountPaid = Double.valueOf(inputAmount.getText());
 							
 				if(PaymentDialog.this.amountPaid < PaymentDialog.this.totalAmount) {
-					PaymentDialog.this.addPaymentError("You gave less than you have to pay");
+					PaymentDialog.this.addPaymentError("You gave less than you have to pay. Please pay at least £" + PaymentDialog.this.totalAmount + ".");
 					return;
 				}
 				
-				PaymentDialog.this.changeAmount = PaymentDialog.this.totalAmount - PaymentDialog.this.amountPaid;
+				
+				PaymentDialog.this.changeAmount = PaymentDialog.this.amountPaid - PaymentDialog.this.totalAmount;
+				
+				if(PaymentDialog.this.changeAmount < 0.01) {
+					PaymentDialog.this.changeAmount = 0.0;
+				}
+				
+				PaymentDialog.this.printReceipt();
+				PaymentDialog.this.openNewOrderWindow();
 				
 			}
 			
 		});
 		
 		form.add(inputAmount);
+		form.add(payButton);
 		
 		return form;
 	}
@@ -144,6 +169,51 @@ public class PaymentDialog extends JDialog {
 	}
 	
 	private void addPaymentError(String error) {
-		return;
+		this.errorMessageLabel.setText(error);
+	}
+	
+	private void openNewOrderWindow() {
+		this.parent.dispose();
+		
+		try {
+			OrderScreen frame = new OrderScreen();
+			frame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void printReceipt() {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		
+		System.out.println("RECEIPT");
+		System.out.println(dateFormat.format(date));
+		System.out.println("Order #" + this.order.getNumber());
+		System.out.println("Table number: " + this.order.getTable().getNumber());
+		System.out.println("--------------------");
+		
+		int i = 1;
+		for(Diner diner : this.order.getDiners()) {
+			System.out.println("-- DINER " + i + " --");
+			
+			for(Course course : diner.getCourses()) {
+				System.out.println(course.getName() + " - " + course.getKiloCalories() + "kcal - £" + "" + course.getPrice());
+			}
+			
+			System.out.println();
+			System.out.println("Total cost for diner " + i + ": £" + OrderScreen.DECIMAL_FORMAT.format(diner.getTotalPrice()));
+			System.out.println("Total kilocalories for diner " + i + ": " + diner.getTotalKiloCalories() + "kcal");
+			System.out.println();
+			
+			i++;
+		}
+		
+		System.out.println("--------------------");
+		System.out.println("Total: £" + OrderScreen.DECIMAL_FORMAT.format(this.totalAmount));
+		System.out.println("Paid: £" + OrderScreen.DECIMAL_FORMAT.format(this.amountPaid));
+		System.out.println("Change: £" + OrderScreen.DECIMAL_FORMAT.format(this.changeAmount));
+		System.out.println("--------------------");
+		System.out.println("Thank you for your custom");
 	}
 }
