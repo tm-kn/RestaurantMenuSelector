36e399e4e9ee7c9a1cbb886483162923abf1f5a0
Tomasz Knapik <tmkn0808@gmail.com>
2016-04-13 13:05:49 +0100

    Add MenuTestCase

---
+++ src/tests/MenuTestCase.java
@@ -0,0 +1,37 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import exceptions.EmptyMenuException;
+import models.Menu;
+
+/**
+ * Tests related to Menu class.
+ * 
+ * @author TJ Knapik <u1562595@unimail.hud.ac.uk>
+ *
+ */
+public class MenuTestCase {
+	
+	private Menu menu;
+	
+	/**
+	 * Set up an empty Menu object.
+	 */
+	@Before
+	public void before() {
+		this.menu = new Menu();
+	}
+	
+	/**
+	 * Test if getting courses from an empty Menu instance will throw EmptyMenuException.
+	 */
+	@Test(expected=EmptyMenuException.class)
+	public void testOpenEmptyMenuRaisesException() {
+		this.menu.getCourses();
+	}
+
+}
